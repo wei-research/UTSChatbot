d4f91b735423c0f6aed46e2362773151bf8537a5
Christian Cabato <chris.cabato@gmail.com>
2021-08-26 16:46:21 +1000

    [CC] Working database query within Rasa X and NLU data integration from current system

--- actions/actions.py
+++ actions/actions.py
@@ -11,24 +11,6 @@ from rasa_sdk.executor import CollectingDispatcher
 import sqlite3
 from sqlite3 import Error
 
-class QueryCourseName(Action):
-
-    def name(self) -> Text:
-        return "query_course_name"  #name of action
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        conn = create_connection("../uts.db")
-
-        slot_value = tracker.get_slot("code")   #get slot value?
-        #slot_name = "name" #if using name column in courses
-        get_query_results = select_course(conn, slot_value)
-        dispatcher.utter_message(text=get_query_results)
-
-        return []
-
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by the db_file
@@ -43,6 +25,25 @@ def create_connection(db_file):
 
     return conn
 
+class QueryCourseName(Action):
+
+    def name(self) -> Text:
+        return "query_course_name"  #name of action
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        #result = init_code(dispatcher, tracker, domain)
+
+        conn = create_connection("./uts.db")
+
+        slot_value = tracker.get_slot("code")   #get slot value?
+        #slot_name = "name" #if using name column in courses
+        get_query_results = select_course(conn, slot_value)
+        dispatcher.utter_message(text=str(get_query_results))
+
+        return
+
 def select_course(conn, value):
     """
     Query tasks by priority
@@ -51,12 +52,18 @@ def select_course(conn, value):
     :return:
     """
     cur = conn.cursor()
-    cur.execute("SELECT * FROM courses WHERE name LIKE ?", ('%'+value+'%',))
-    #cur.execute(f"""SELECT * FROM courses WHERE name='{value}'"""") #sample fstring
+    cur.execute("SELECT * FROM courses WHERE course_id LIKE ?", ('%'+str(value)+'%',))
+    #cur.execute(f"""SELECT * FROM courses WHERE name='{value}'""") #sample fstring
 
+    #cur.execute("SELECT * FROM courses WHERE name LIKE '%Science%'")
     rows = cur.fetchall()
-
-    for row in rows:
-        #print(row) #print all rows from database
-        return[print(f"UTS offers {row[1]} at {row[6]}.")]
+    print(rows)
+    
+    if len(list(rows)) < 1:
+        return "There are no matches for your query."
+    else:
+        for row in rows:
+            #print(row) #print all rows from database
+            #return[print(f"UTS offers {row[1]} at {row[6]}.")]
+            return f"UTS offers {row[1]} at {row[6]}."
 
